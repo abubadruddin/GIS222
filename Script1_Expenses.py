a9d94168c983058cdc457992c52dc24d961fc655
Abu Badruddin <azmbadruddin@gmail.com>
2018-07-22 16:35:47 -0400

    added 1st script

---
+++ Script1_Expenses.py
@@ -0,0 +1,10 @@
+#Abu badruddin
+##Date: February 1, 2016
+### 1st Exercise
+
+x = input ("Enter your Grocery bill:  ")
+y = input ("Enter your phone bill:  ")
+
+c = x + y
+
+print "Your total exoenses are: " + str(c)
